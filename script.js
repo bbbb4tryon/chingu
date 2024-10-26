f1c0bc7c39bc92fab4a269787cf85b6be1e86c28
bbb4tryon <b.t.tryon@gmail.com>
2024-10-26 17:39:26 -0600

    add basic sections and forms for location, training, weather

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("JS is linked to this correctly")
+
+document.getElementByID("packingForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const location = document.getElementById("location").value
+    const adveture = document.getElementById("adventure").value
+    const time = document.getElmentById("weather").value
+
+    const simulatedPackingSuggestions = getPackingList(adventure, weather);
+
+    const packingList = document.getElementById("list");
+    packingList.innerHTML = ""; // clears previous packingList
+    packingSuggestions.forEach(item => {
+        const listItem = document.createElement("li")
+        listItem.textContext = item;
+        packingList.appendChild(listItem)
+    });
+});
+
+function getPackingList(adventure, time ) {
+    const baseItems = ["1 Liter Water per 1 hour activity snacks", "snacks", "hat"]
+    if (adventure == swimming) {
+        baseItems.push("googles")
+    } else if (adventure == biking) {
+        baseItems.push("electrolytes", "sunglasses")
+    } else if (adventure == running) {
+        baseItems.push("running socks")
+    }
+
+    if (weather == "cold") {
+baseItems.push("full gloves")
+    } else if (weather == "warmer") {
+baseItems.push("spray sunscreen")
+    }
+
+    return baseItems
+}
+
+function fetchWeatherCondition(location) {
+    const fakeapiKey = 'REPLACE_WITH YOURACTUAL_KEY'
+}
